6150830800aa00ca4f51d3439e2ab68929958d1b
Huzaifa Hassan <huzaifahassan97@outlook.com>
2025-10-16 04:53:30 +0100

    todayscreen

--- lib/todayscreen.dart
+++ lib/todayscreen.dart
@@ -8,12 +8,73 @@ class Todayscreen extends StatefulWidget {
 }
 
 class _TodayscreenState extends State<Todayscreen> {
+  double screenHeight_ = 0;
+  double screenWidth_ = 0;
+
+
   @override
   Widget build(BuildContext context) {
+    screenHeight_ = MediaQuery
+        .of(context)
+        .size
+        .height;
+    screenWidth_ = MediaQuery
+        .of(context)
+        .size
+        .width;
     return  Scaffold(
-      body:Center(
-        child:Text("TodayScreen"),
-      ),
+      body:SingleChildScrollView(
+        padding:EdgeInsets.all(20),
+        child:Column(
+          children:[
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top:32),
+              child:Text(
+                "Welcome",
+                style:TextStyle(
+                  color: Colors.black54,
+                  fontSize: screenHeight_ / 35,
+              ),
+            ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child:Text(
+                "Employee",
+                style:TextStyle(
+                  color: Colors.black54,
+                  fontSize: screenHeight_ / 25 ,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top:32),
+              child:Text(
+                "Today's Status",
+                style:TextStyle(
+                  color: Colors.black54,
+                  fontSize: screenHeight_ / 25 ,
+                ),
+              ),
+            ),
+            Container(
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  color:Colors.black26,
+                  blurRadius: 10,
+                    offset: Offset(2,2),
+                   ),
+               ],
+              ),
+            ),
+          ],
+        ),
+      )
 
     );
   }
